ce669b3a71bbc4d1562ec4f0c6421ce3deec7f57
Yaskovets Evgen <sinichkabird@mail.ru>
2020-09-11 21:14:01 +0300

    first commit

---
+++ yaskovets.py
@@ -0,0 +1,13 @@
+a = "Не знаю, как там в Лондоне, я не была. Может, там собака — друг человека. А у нас управдом — друг человека!"
+print(len(a))
+print(a[::-1])
+print(str.upper(a))
+print(str.title(a))
+b = a.count("нд")
+print(b)
+a.count("ам")
+c = a.count("о")
+print(c)
+b = 1
+c = 7
+print(b < c)
