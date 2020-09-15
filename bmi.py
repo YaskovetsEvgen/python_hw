2e16b40aa481c83f57e29685227c4069e4367032
YaskovetsEvgen <63747888+YaskovetsEvgen@users.noreply.github.com>
2020-09-16 02:53:38 +0300

    Delete bmi.py

--- bmi.py
+++
@@ -1,8 +0,0 @@
-a = float(input("weight,kg:"))
-b = float(input("height,m:"))
-# calculation BMI
-c = (a/(b**2))
-# output scale
-d = float(c-20)
-e = float(50-c)
-print("20" + round(d)*"=" + "|" + round(e)*"=" + "50")
